3d1e4fe8b0b79d05598fdbf903b9ae895860c4b3
d-banana <mail@nolan.contact>
2024-03-20 18:01:30 +0100

    Basic implementation with useContext and useState for updating/rendering a view TODO need to refactor code and maybe formalize it(and removing typescript)

--- crates/web/app/src/App.tsx
+++ crates/web/app/src/App.tsx
@@ -1,115 +1,31 @@
 import "@mantine/core/styles.css";
-import {
-        ActionIcon,
-        Badge,
-        Button,
-        Card,
-        Center,
-        Container,
-        Divider,
-        Group,
-        Image,
-        Input,
-        List,
-        MantineProvider,
-        SegmentedControl,
-        Text,
-} from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
-import img from "./logo-banner-001.png";
 import {init, update, render_todo} from "wasm";
+import {useState, createContext} from 'react';
+import Todo from "./Todo.tsx";
+
+export const ViewWasmContext = createContext(null);
+export const SetViewWasmContext = createContext(() =>  {});
+const model = init();
 
 export default function App() {
-        let model= init();
+        const [view, _setView] = useState(render_todo(model));
+        let setView = (message) => {
+                update(model, message);
+                _setView(render_todo(model));
+        } ;
+
+        /*let model = init();
         update(model, {Todo:{UpdateTaskNewTitle: "test"}});
-        console.log(render_todo(model));
+        console.log(render_todo(model));*/
         return (
                 <MantineProvider theme={theme}>
-                        <Container size={500} mt="lg">
-                                <Card shadow="sm" padding="none" radius="md" withBorder>
-                                        <Card.Section>
-                                                <Image
-                                                        src={img}
-                                                        height={200}
-                                                        fit="cover"
-                                                        alt="noobi.fi logo"
-                                                />
-                                        </Card.Section>
-                                        <SegmentedControl
-                                                fullWidth
-                                                withItemsBorders={false}
-                                                data={['📅 low', '⏳ medium', '💣 high']}
-                                                color="orange"
-                                                mb="sm"
-                                        />
-                                        <Input
-                                                me="md"
-                                                ms="md"
-                                                placeholder="Task name"
-                                        />
-                                        <Center me="xl" ms="xl">
-                                                <Button
-                                                        mt="lg"
-                                                        mb="sm"
-                                                        color="orange"
-                                                        fullWidth
-                                                        radius="md"
-                                                >
-                                                        💾 Add a task
-                                                </Button>
-                                        </Center>
-                                        <Divider my="md" mb="xl" />
-                                        <List
-                                                ml="md"
-                                                mr="md"
-                                                mb="sm"
-                                                spacing="md"
-                                                styles={{
-                                                        itemWrapper: {width: "100%"},
-                                                        itemLabel: {flexGrow: 1}
-                                                }}
-                                        >
-                                                <List.Item
-                                                        icon={
-                                                                <Badge color="orange">📅 low</Badge>
-                                                        }
-                                                >
-                                                        <Group justify="space-between" wrap="nowrap">
-                                                                <Text lineClamp={1} inline>
-                                                                        Wash dishes
-                                                                </Text>
-                                                                <Group justify="space-between" wrap="nowrap">
-                                                                        <ActionIcon me="auto" variant="light" color="orange" aria-label="Edit">
-                                                                                ✏️
-                                                                        </ActionIcon>
-                                                                        <ActionIcon me="auto" variant="light" color="orange" aria-label="Delete">
-                                                                                🗑
-                                                                        </ActionIcon>
-                                                                </Group>
-                                                        </Group>
-                                                </List.Item>
-                                                <List.Item
-                                                        icon={
-                                                                <Badge color="orange">💣 high</Badge>
-                                                        }
-                                                >
-                                                        <Group justify="space-between" wrap="nowrap">
-                                                                <Text lineClamp={1} inline>
-                                                                        Pay bills
-                                                                </Text>
-                                                                <Group justify="space-between" wrap="nowrap">
-                                                                        <ActionIcon me="auto" variant="light" color="orange" aria-label="Edit">
-                                                                                ✏️
-                                                                        </ActionIcon>
-                                                                        <ActionIcon me="auto" variant="light" color="orange" aria-label="Delete">
-                                                                                🗑
-                                                                        </ActionIcon>
-                                                                </Group>
-                                                        </Group>
-                                                </List.Item>
-                                        </List>
-                                </Card>
-                        </Container>
+                        <ViewWasmContext.Provider value={view}>
+                                <SetViewWasmContext.Provider value={setView}>
+                                        <Todo />
+                                </SetViewWasmContext.Provider>
+                        </ViewWasmContext.Provider>
                 </MantineProvider>
         );
 }
---
+++ crates/web/app/src/Todo.tsx
@@ -0,0 +1,112 @@
+import {
+        ActionIcon,
+        Badge,
+        Button,
+        Card,
+        Center, Container,
+        Divider,
+        Group,
+        Image,
+        Input,
+        List,
+        SegmentedControl,
+        Text
+} from "@mantine/core";
+import img from "./logo-banner-001.png";
+import {useContext} from "react";
+import {SetViewWasmContext, ViewWasmContext} from "./App.tsx";
+
+export default function Todo() {
+        const view = useContext(ViewWasmContext);
+        const setView = useContext(SetViewWasmContext);
+
+        return (
+                <Container size={500} mt="lg">
+                        <Card shadow="sm" padding="none" radius="md" withBorder>
+                                <Card.Section>
+                                        <Image
+                                                src={img}
+                                                height={200}
+                                                fit="cover"
+                                                alt="noobi.fi logo"
+                                        />
+                                </Card.Section>
+                                <SegmentedControl
+                                        fullWidth
+                                        withItemsBorders={false}
+                                        data={['📅 low', '⏳ medium', '💣 high']}
+                                        color="orange"
+                                        mb="sm"
+                                />
+                                <Input
+                                        me="md"
+                                        ms="md"
+                                        placeholder="Task name"
+                                        value={view.task_new_title}
+                                        onChange={(event) => setView({Todo:{UpdateTaskNewTitle: event.currentTarget.value}})}
+                                />
+                                <Center me="xl" ms="xl">
+                                        <Button
+                                                mt="lg"
+                                                mb="sm"
+                                                color="orange"
+                                                fullWidth
+                                                radius="md"
+                                        >
+                                                💾 Add a task
+                                        </Button>
+                                </Center>
+                                <Divider my="md" mb="xl" />
+                                <List
+                                        ml="md"
+                                        mr="md"
+                                        mb="sm"
+                                        spacing="md"
+                                        styles={{
+                                                itemWrapper: {width: "100%"},
+                                                itemLabel: {flexGrow: 1}
+                                        }}
+                                >
+                                        <List.Item
+                                                icon={
+                                                        <Badge color="orange">📅 low</Badge>
+                                                }
+                                        >
+                                                <Group justify="space-between" wrap="nowrap">
+                                                        <Text lineClamp={1} inline>
+                                                                Wash dishes
+                                                        </Text>
+                                                        <Group justify="space-between" wrap="nowrap">
+                                                                <ActionIcon me="auto" variant="light" color="orange" aria-label="Edit">
+                                                                        ✏️
+                                                                </ActionIcon>
+                                                                <ActionIcon me="auto" variant="light" color="orange" aria-label="Delete">
+                                                                        🗑
+                                                                </ActionIcon>
+                                                        </Group>
+                                                </Group>
+                                        </List.Item>
+                                        <List.Item
+                                                icon={
+                                                        <Badge color="orange">💣 high</Badge>
+                                                }
+                                        >
+                                                <Group justify="space-between" wrap="nowrap">
+                                                        <Text lineClamp={1} inline>
+                                                                Pay bills
+                                                        </Text>
+                                                        <Group justify="space-between" wrap="nowrap">
+                                                                <ActionIcon me="auto" variant="light" color="orange" aria-label="Edit">
+                                                                        ✏️
+                                                                </ActionIcon>
+                                                                <ActionIcon me="auto" variant="light" color="orange" aria-label="Delete">
+                                                                        🗑
+                                                                </ActionIcon>
+                                                        </Group>
+                                                </Group>
+                                        </List.Item>
+                                </List>
+                        </Card>
+                </Container>
+        )
+}
